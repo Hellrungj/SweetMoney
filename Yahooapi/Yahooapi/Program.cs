ea273c81eca9808624fdaced793f171f7d7769d3
Alexandrith C Sharron <alexsharron@gmail.com>
2017-02-25 07:10:39 -0500

    Updated URL Get request for Quries to Yahoo finance

--- Yahooapi/Yahooapi/Program.cs
+++ Yahooapi/Yahooapi/Program.cs
@@ -10,57 +10,46 @@ using System.Diagnostics;
 namespace Yahooapi
 {
 
-    class Program
+    class Program : WebClient
     {
         static void Main(string[] args)
-        { 
-            Console.WriteLine("Write in the stock you want");
-            var userstock = Console.ReadLine().ToUpper();
+        {
 
-            Console.WriteLine("Please enter start day");
-            var startday = Console.ReadLine();
-
-            Console.WriteLine("please enter start month");
-            var startmonth = Console.ReadLine();
-
-            Console.WriteLine("enter start year");
-            var startyear = Console.ReadLine();
-
-            Console.WriteLine("enter end day");
-            var endday = Console.ReadLine();
-
-            Console.WriteLine("enter end month");
-            var endmonth = Console.ReadLine();
-
-            Console.WriteLine("enter end year");
-            var endyear = Console.ReadLine();
-
-            var urlPrototype = @"http://ichart.finance.yahoo.com/table.csv?s={0}&a{1}&b={2}&c={3}&d={4}&e={5}&f={6}&g=d&ignore=.csv";
-
-            var url = string.Format(urlPrototype, userstock, startmonth, startday, startyear, endmonth, endday, endyear);
-            Console.WriteLine(url);
 
             //var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+            var url = "https://finance.yahoo.com/quote/AAPL?p=AAPL";
+            // Create a request for the URL.   
+            WebRequest request = WebRequest.Create(
+              url);
+            // If required by the server, set the credentials.  
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
 
-            WebRequest wrGETURL;
-            wrGETURL = WebRequest.Create(url);
+            int one = 1;
 
-            Stream objStream;
-            objStream = wrGETURL.GetResponse().GetResponseStream();
-
-            StreamReader objReader = new StreamReader(objStream);
-
-            string sLine = "";
-            int i = 0;
-
-            while (sLine != null)
+            foreach (var item in responseFromServer)
             {
-                i++;
-                sLine = objReader.ReadLine();
-                if (sLine != null)
-                    Console.WriteLine("{0}:{1}", i, sLine);
+                if (one == 1)
+                {
+                    Console.WriteLine(item);
+                    one = one + 1;
+                }
             }
-            Console.ReadLine();
+            // Display the content.  
+            //Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.  
+            reader.Close();
+            response.Close();
+
         }
     }
 }
